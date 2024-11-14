b0ecc443af6171c9343a5371ebaf9dd607a18dea
cloudimpl <github.bot@cloudimpl.com>
2024-11-14 02:01:58 +0000

    Update from https://github.com/cloudimpl/next-coder/commit/65345e0bdaf01c2f4276077e7019fc9a0078944f

--- polycode/logger.go
+++ polycode/logger.go
@@ -1,7 +1,7 @@
 package polycode
 
 import (
-	"sync"
+	"encoding/json"
 	"time"
 )
 
@@ -23,8 +23,7 @@ type LogMsg struct {
 }
 
 type LogEntry struct {
-	msg       *LogMsg
-	publisher LogPublisher
+	msg *LogMsg
 }
 
 func (entry *LogEntry) Str(key string, val string) *LogEntry {
@@ -49,91 +48,57 @@ func (entry *LogEntry) Bool(key string, val bool) *LogEntry {
 
 func (entry *LogEntry) Done() {
 	entry.msg.Timestamp = time.Now().UnixMicro()
-	_ = entry.publisher.Publish(*entry.msg)
+	logJson, err := json.Marshal(entry.msg)
+	if err == nil {
+		println(string(logJson))
+	}
 }
 
 func (entry *LogEntry) Msg(msg string) {
 	entry.msg.Message = msg
-	entry.msg.Timestamp = time.Now().UnixMicro()
-	_ = entry.publisher.Publish(*entry.msg)
+	entry.Done()
 }
 
-type Logger struct {
-	section   string
-	publisher LogPublisher
+type Logger interface {
+	Log(level LogLevel) *LogEntry
+	Debug() *LogEntry
+	Info() *LogEntry
+	Warn() *LogEntry
+	Error() *LogEntry
 }
 
-func (logger *Logger) Log(level LogLevel) *LogEntry {
+type JsonLogger struct {
+	section string
+}
+
+func (logger *JsonLogger) Log(level LogLevel) *LogEntry {
 	return &LogEntry{
 		msg: &LogMsg{
 			Level:   level,
 			Section: logger.section,
 			Tags:    make(map[string]interface{}),
 		},
-		publisher: logger.publisher,
 	}
 }
 
-func (logger *Logger) Debug() *LogEntry {
+func (logger *JsonLogger) Debug() *LogEntry {
 	return logger.Log(DebugLevel)
 }
 
-func (logger *Logger) Info() *LogEntry {
+func (logger *JsonLogger) Info() *LogEntry {
 	return logger.Log(InfoLevel)
 }
 
-func (logger *Logger) Warn() *LogEntry {
+func (logger *JsonLogger) Warn() *LogEntry {
 	return logger.Log(WarnLevel)
 }
 
-func (logger *Logger) Error() *LogEntry {
+func (logger *JsonLogger) Error() *LogEntry {
 	return logger.Log(ErrorLevel)
 }
 
-type LogPublisher interface {
-	Publish(msg LogMsg) error
-	PublishList(msgList []LogMsg) error
-	Flush(process func(messages []LogMsg) error) error
-}
-
-type LogAggregator struct {
-	messages []LogMsg
-	mu       sync.Mutex
-}
-
-func (l *LogAggregator) PublishList(msgList []LogMsg) error {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	l.messages = append(l.messages, msgList...)
-	return nil
-}
-
-func (l *LogAggregator) Publish(msg LogMsg) error {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	l.messages = append(l.messages, msg)
-	return nil
-}
-
-func (l *LogAggregator) Flush(process func(messages []LogMsg) error) error {
-	l.mu.Lock()
-	defer l.mu.Unlock()
-	err := process(l.messages)
-	if err != nil {
-		return err
+func CreateLogger(section string) Logger {
+	return &JsonLogger{
+		section: section,
 	}
-
-	l.messages = make([]LogMsg, 0)
-	return nil
-}
-
-func CreateLogger(section string, publisher LogPublisher) *Logger {
-	return &Logger{
-		section:   section,
-		publisher: publisher,
-	}
-}
-
-func CreateLogAggregator() *LogAggregator {
-	return &LogAggregator{messages: make([]LogMsg, 0)}
 }
